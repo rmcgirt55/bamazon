1a7df9c3f7e01b7b8fb72a420e9d4bfba97a5869
richard mcgirt <richardmcgirt@richards-MBP.attlocal.net>
2019-11-03 20:03:09 -0500

    first commit

---
+++ bamazon.sql
@@ -0,0 +1,43 @@
+    DROP DATABASE IF EXISTS bamazon_db;
+    CREATE DATABASE bamazon_db;
+
+    Use bamazon_db;
+
+    CREATE TABLE products (
+        item_id INT AUTO_INCREMENT NOT NULL,
+        Product_name VARCHAR(100) NULL,
+        department_name VARCHAR(100) NULL,
+        price DECIMAL(10,2) NULL,
+        stock_quality INT NULL,
+        product_sales DECEMAL(10,2) DEFAULT 0,
+        PRIMARY KEY (item_id)
+
+    );
+
+    Insert INTO products (product_name, department_name, price, stock_quality, product_sales)
+    Values ("Prince's Purple Rain - Vinyl", "Music", 25.99, 100, 4000), ("The Breakfeast Club", "Movies", 19.99, 5, 10),
+    ("Boosted Board", "Toys", 15000, 56, 500), ("fidget spinner", "Toys", 9.99, 20, 100),
+    ("Quip", "Toiletries", 25, 45, 2000), ("20g CO2 Tank", "Hobby", 35.99, 5, 15), ("Hammer", "Tools", 5.99, 34, 345),
+    ("Xbox One", "Electronics", 299.99, 34, 456), ("Slavin Jersey", "Sports Appearel", 199.99, 5, 100),
+    ("PS4", "Electronics", 299.99, 34, 456);
+
+    CREATE TABLE departments (
+        department_id INT AUTO_INCREMENT NOT NULL,
+        department_name VARCHAR(100) NULL,
+        OVER_head_costs DECIMAL(10,2) NULL,
+        PRIMARY KEY (department_id)
+    );
+
+INSERT INTO departments (department_name, over_head_costs)
+Values ("Music", 1000), ("Movies", 200), ("Toys", 1000), ("Toiletries", 5000), ("Tools", 1000), ("Sports Appearel", 5000), ("Electronics", 3000);
+
+SELECT * FROM products;
+SELECT * FROM departments;
+
+SELECT department_id, departments.department_name, over_head_costs, SUM(product_sales) AS product_sales,
+SUM(product_sales) - over_head_costs AS total_profit
+FROM departments
+INNER JOIN products
+ON departments.department_name = products.department_name
+GROUP BY department_id;
+
---
+++ bamazonCustomer.js
@@ -0,0 +1,135 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+var consoleTableNPM = require("console.table");
+var pw = require("./pw.js");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 404,
+    user: "root",
+    password: root,
+    database: "bamazon_db"
+});
+
+connection.connect(function(error){
+    if (error) throw error;
+
+    console.log("\n**************************"
+        + "\nWork Harder. Have more Fun. Make things historic! Welcome to Bamazon!\n"
+        +
+        "**************************\n");
+welcome();
+});
+
+function welcome() {
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            choices: ["Browse items", "Exit the app"],
+            message: "choice one of the choices"
+        }
+    ]).then(function(action) {
+        if (action.action === "Browse items") {
+            BrowseItems();
+        } else if (action.action === "Exit the app") {
+            Exit();
+        }
+    });
+}
+
+function BrowseItems() {
+    var query = "SELECT * FROM products";
+    connection.query(query, function(error, results){
+        if (error) throw error;
+consoleTable(results);
+inquirer.prompt([
+    {
+        name: "id",
+        message: "Please tell us the Id of the item you would like to buy.",
+        validate: function(value) {
+            if (value > 0 && is NaN(value) === false && value <= results.length) { return true;
+            }
+            return false;
+        }
+    },
+    {
+        name: "quantity",
+        message: "HOw many do you want?",
+        validate: function(value) {
+            if (value > 0 && isNaN(value) === false) {
+                return true;
+            }
+            return false;
+        }
+        }
+]).then(function(transaction) {
+    var itemquantity;
+    var itemPrice;
+    var itemName;
+    var productSales;
+
+    for (var j = 0; j < results.length; j++) {
+        if (parseInt(transaction.id) === results[j].item_id) {
+            itemquantity = results[j].stock_quantity;
+            itemPrice = results[j].price;
+            itemName = results[j].product_name;
+            productSales = results[j].product_sales;
+        }
+    }
+if (parseInt(transaction.qty) > itemquantity) {
+    console.log("\nWE do not have that many. We only have " + itemquantity + " in stock.\n");
+    welcome();
+}
+
+else if (parseInt(transaction.quantity) <= itemquantity) {
+    console.log("\nOrder complete " + transaction.quantity + " of " + itemName + ".");
+    lowerQuanity(transaction.id, transaction.quantity, itemquantity, itemPrice);
+    salesRevenue(transaction.id, transaction.quantity,productSales, itemPrice);
+}
+});
+    });
+}
+
+functionconsoleTable(results) {
+    var values = [];
+    for(var i = 0; i < results.length; i++)
+ {
+     var resultObject = {
+         ID: results[i].item_id,
+         Item: results[i].product_name,
+         Price: "$" + results[i].price
+     };
+     values.push(resultObject);
+ }
+ console.table("\nItems for sale", values);
+}
+
+function lowerQuanity(item, purchasequantity, stockQTY, price) {
+    connection.query(
+        ".",
+        [
+            {
+                stock_quantity: stock_quantity - parseInt(purchasequantity)
+            },
+            {
+                iten_id: parseInt(item)
+            }
+        ],
+        function(error, response) {
+            if (error) throw error;
+        });
+    }
+
+    function salesRevenue(item, purchasequantity, productSales, price) { var customerCost = parseInt(purchasequantity) * price;
+    connection.query(
+        ".",
+        [ 
+            {
+                produc_sales: productSales + customerCost
+            },
+            {
+                item_id: parseInt(item)
+            }
+        ],
+    )}
